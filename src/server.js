b4d6e17eadf0c1d807d0150e8bb509fd6cf85084
Jaro111 <jarokark@protonmail.com>
2024-02-05 15:22:33 +0000

    enhancement: website example complete

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+// app -object
+// use - method
+// "example" - folder
+// app.use("/example", express.static("example"));
+
+app.use("/myWebsite", express.static("website"));
+
 
 app.get ("/health", (req, res) => {
     res.send("healthy");
@@ -9,7 +16,7 @@ app.get ("/health", (req, res) => {
 
 app.listen(5001, () => {
     console.log("Server is listening on port 5001")
-})
+});
 
 
 
