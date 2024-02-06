60d16e887f421083a2b90c9665f293d0add0214c
Jaro111 <jarokark@protonmail.com>
2024-02-06 10:15:43 +0000

    some changes

--- src/server.js
+++ src/server.js
@@ -5,18 +5,17 @@ const app = express();
 // app -object
 // use - method
 // "example" - folder
-// app.use("/example", express.static("example"));
+app.use("/example", express.static("example"));
 
 app.use("/myWebsite", express.static("website"));
 
+app.get("/health", (req, res) => {
+  res.send("healthy");
+});
+// *************************************************************************
 
-app.get ("/health", (req, res) => {
-    res.send("healthy");
-})
+// *************************************************************************
 
 app.listen(5001, () => {
-    console.log("Server is listening on port 5001")
+  console.log("Server is listening on port 5001");
 });
-
-
-
